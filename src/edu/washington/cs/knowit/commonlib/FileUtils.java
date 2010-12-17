30733a7f78fb812300374a3a0ed6fa088d8d9632
Michael Schmitz <michael@schmitztech.com>
2010-12-17 21:11:00 +0000

    Added some comments.

--- src/edu/washington/cs/knowit/commonlib/FileUtils.java
+++ src/edu/washington/cs/knowit/commonlib/FileUtils.java
@@ -4,6 +4,11 @@ import java.io.*;
 import java.util.*;
 
 public class FileUtils {
+	/***
+	 * Get the text after the final dot (.) in the file name.
+	 * @param f The file to get the extension of.
+	 * @return The extension of File f.
+	 */
     public static String getExtension(File f) {
         String ext = null;
         
@@ -17,10 +22,23 @@ public class FileUtils {
         return ext;
     }
     
+    /***
+     * Writes all lines read from the reader.
+     * @param reader The source reader
+     * @param writer The destination writer
+     * @throws IOException
+     */
     public static void pipe(Reader reader, Writer writer) throws IOException {
         pipe(reader, writer, 1024);
     }
     
+    /***
+     *  Writes all lines read from the reader.
+     * @param reader the source reader
+     * @param writer the destination writer
+     * @param buffersize size of the buffer to use
+     * @throws IOException
+     */
     public static void pipe(Reader reader, Writer writer, int buffersize) throws IOException {
         char[] buffer = new char[1024];
         while (reader.read(buffer) != -1) {
@@ -28,12 +46,24 @@ public class FileUtils {
         }
     }
     
+    /***
+     * Return all files beneath path.
+     * @param path the path to search
+     * @param recursive iff true, search subdirectories too.
+     * @return
+     */
     public static ArrayList<File> Find(File path, Boolean recursive) {
         ArrayList<File> files = new ArrayList<File>();
         Find(files, path, recursive);
         return files;
     }
     
+    /***
+     * A private helper function for building the results for public Find.
+     * @param files
+     * @param path
+     * @param recursive
+     */
     private static void Find(List<File> files, File path, Boolean recursive) {
         if (path.isDirectory()) {
             // iterate over files
