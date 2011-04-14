5b585102157daffb1a63f5e331715580a4cb1747
Michael Schmitz <michael@schmitztech.com>
2011-04-14 16:41:58 +0000

    Adding ComparableSequence.java

---
+++ src/main/java/edu/washington/cs/knowitall/commonlib/ComparableSequence.java
@@ -0,0 +1,45 @@
+package edu.washington.cs.knowitall.commonlib;
+
+public class ComparableSequence<E extends Comparable<E>> implements Comparable<ComparableSequence<E>> {
+    private final E[] comparables;
+    
+    public ComparableSequence(E[] comparables) {
+        this.comparables = comparables;
+    }
+    
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        else if (other == null) {
+            return false;
+        }
+        else if (! (other instanceof ComparableSequence<?>)) {
+            return false;
+        }
+        
+        @SuppressWarnings("unchecked")
+        ComparableSequence<E> comperable = (ComparableSequence<E>)other;
+        return this.compareTo(comperable) == 0;
+    }
+
+    @Override
+    public int compareTo(ComparableSequence<E> comparable) {
+        return this.compareTo(comparable, 0);
+    }
+    
+    public int compareTo(ComparableSequence<E> comparable, int index) {
+        int compare = this.comparables[index].compareTo(comparable.comparables[index]);
+        if (compare == 0) {
+            if (index == comparables.length - 1) {
+                return 0; // equal
+            }
+            else {
+                return this.compareTo(comparable, index + 1);
+            }
+        }
+        else {
+            return compare;
+        }
+    }
+}
