dff26feaacb5d22b42bb5ffa53697e1b3dfde898
Michael Schmitz <michael@schmitztech.com>
2011-09-08 16:14:40 +0000

    Added some comments.

--- src/main/java/edu/washington/cs/knowitall/commonlib/FileUtils.java
+++ src/main/java/edu/washington/cs/knowitall/commonlib/FileUtils.java
@@ -47,6 +47,30 @@ public class FileUtils {
     }
     
     /***
+     * Writes all lines read from the reader.
+     * @param is The input stream
+     * @param os The output stream
+     * @throws IOException
+     */
+    public static void pipe(InputStream is, OutputStream os) throws IOException {
+        pipe(is, os, 4092);
+    }
+    
+    /***
+     *  Writes all lines read from the reader.
+     * @param is The input stream
+     * @param os The output stream
+     * @param buffersize size of the buffer to use
+     * @throws IOException
+     */
+    public static void pipe(InputStream is, OutputStream os, int buffersize) throws IOException {
+        byte[] buffer = new byte[buffersize];
+        while (is.read(buffer) != -1) {
+            os.write(buffer);
+        }
+    }
+    
+    /***
      * Return all files beneath path.
      * @param path the path to search
      * @param recursive iff true, search subdirectories too.
